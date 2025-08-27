35d7b7978617779e49cb2d29f48645937a8a296b
Boamah Evans Koranteng <legaltechrescue@gmail.com>
2025-08-27 18:54:23 +0000

    Initial commit: Vite React project setup

---
+++ src/recipeStore.js
@@ -0,0 +1,6 @@
+import { create } from 'zustand';
+
+export const useRecipeStore = create((set) => ({
+  recipes: [],
+  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
+}));
